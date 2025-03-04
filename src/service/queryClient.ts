5a337b89048c209e0d0c2d495b1c1aa4f6c540b9
Alvin John <ajohn@deeprisk.ai>
2025-03-04 11:43:11 -0600

    feat: Update QueryClient configuration with enhanced refetching options

--- src/service/queryClient.ts
+++ src/service/queryClient.ts
@@ -3,9 +3,15 @@ import { QueryClient, QueryClientConfig } from "@tanstack/react-query";
 const QueryClientParams: QueryClientConfig = {
   defaultOptions: {
     queries: {
-      refetchInterval: 1000 * 10,
+      enabled: true,
       staleTime: 1000 * 5,
       networkMode: "online",
+      refetchInterval: 1000 * 10,
+      refetchIntervalInBackground: true,
+      refetchOnWindowFocus: true,
+      refetchOnMount: true,
+      refetchOnReconnect: true,
+      gcTime: 1000 * 20,
     },
   },
 };
